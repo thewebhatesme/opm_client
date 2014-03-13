e1ebc27c732e2391196d8e3e1cb72bb7e8c7948d
Nils Langner <nils.langner@phmLabs.com>
2014-03-13 20:41:58 +0100

    Added setup:phantom and setup:config stub

---
+++ src/Whm/Opm/Client/Command/SetupPhantom.php
@@ -0,0 +1,22 @@
+<?php
+namespace Whm\Opm\Client\Command;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Command\Command;
+
+class SetupPhantom extends Command
+{
+
+  protected function configure ()
+  {
+    $this->setName('setup:phantom')
+      ->setDescription('Process an url and send the result (har file) to an opm server.');
+  }
+
+  protected function execute (InputInterface $input, OutputInterface $output)
+  {
+  }
+}
