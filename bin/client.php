b744da82ecaa9ba76b6066a4512b1c8fc40edd43
Nils Langner <nils.langner@phmLabs.com>
2014-03-13 20:23:01 +0100

    Added check for composer run

--- bin/client.php
+++ bin/client.php
@@ -2,6 +2,10 @@
 
 use Whm\Opm\Client\Command\RunMessurement;
 
+if(!file_exists(__DIR__."/../vendor/autoload.php")) {
+   die("\n    You have to run 'composer.phar install' before you can use the client.\n\n");
+}
+
 include_once __DIR__.'/../vendor/autoload.php';
 
 use Whm\Opm\Client\Command\ProcessUrl;
