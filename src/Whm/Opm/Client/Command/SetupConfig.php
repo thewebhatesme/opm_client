fcc1549956e7b983888b7a312284ac146f6582a3
Nils Langner <nils.langner@thewebhatesme.com>
2014-03-14 09:29:40 +0100

    Added setup:config

--- src/Whm/Opm/Client/Command/SetupConfig.php
+++ src/Whm/Opm/Client/Command/SetupConfig.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Whm\Opm\Client\Command;
 
 use Symfony\Component\Console\Output\OutputInterface;
@@ -9,14 +10,66 @@ use Symfony\Component\Console\Command\Command;
 
 class SetupConfig extends Command
 {
+    private $configFileName = "config.yml";
 
-  protected function configure ()
-  {
-    $this->setName('setup:config')
-      ->setDescription('Process an url and send the result (har file) to an opm server.');
-  }
+    protected function configure ()
+    {
+        $this->setName('setup:config')->setDescription('Process an url and send the result (har file) to an opm server.');
+    }
 
-  protected function execute (InputInterface $input, OutputInterface $output)
-  {
-  }
+    protected function execute (InputInterface $input, OutputInterface $output)
+    {
+
+        if( file_exists($_SERVER["PWD"] . "/".$this->configFileName)) {
+            $output->writeln("");
+            $output->writeln("      <error>Config file already exists. Please (re)move it before creating a new one.</error>");
+            $output->writeln("");
+            die;
+        }
+
+        $output->writeln("");
+        $output->writeln("   Open Performance Monitor");
+        $output->writeln("   ========================");
+        $output->writeln("");
+
+        $clientId = readline("   Your client id: ");
+
+        $phantomFound = false;
+        while (! $phantomFound) {
+            $phantom = readline("   Path to phantomjs executable: ");
+            if(!is_file($phantom)) {
+                $output->writeln("");
+                $output->writeln("      <error>Phantomjs not found. Please try again. If you don't have phantomjs installed use client.phar setup:phantom.</error>");
+                $output->writeln("");
+            }else{
+                $phantomFound = true;
+            }
+        }
+
+        $server = readline("   Open Performance Monitor Server (press enter for default server www.linkstream): ");
+
+        if ($server == "")
+            $server = "http://www.linkstream.org";
+
+        $maxConnections = readline("   Parallel Connections (press enter for default: 5): ");
+
+        if ($maxConnections == "")
+            $maxConnections = 5;
+
+        $this->createConfigFile($server, $phantom, $maxConnections, $clientId);
+
+        $output->writeln("");
+        $output->writeln("   <info>Config (".$this->configFileName.") was created.</info>");
+        $output->writeln("");
+    }
+
+    private function createConfigFile($server, $phantom, $maxConnections, $clientId)
+    {
+        ob_start();
+        include __DIR__."/SetupConfig/config.yml";
+        $config = ob_get_contents();
+        ob_end_clean();
+
+        file_put_contents($this->configFileName, $config);
+    }
 }
