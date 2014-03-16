3a25dc3670e9017d7308ada3eec2c491c1b5a3cf
Philipp Bräutigam <philipp.braeutigam@googlemail.com>
2014-03-16 13:32:33 +0100

    Add phpDoc blocks to Phantom.php

--- src/Whm/Opm/Client/Browser/Phantom.php
+++ src/Whm/Opm/Client/Browser/Phantom.php
@@ -7,22 +7,47 @@ use Whm\Opm\Client\Browser\Browser;
 class Phantom implements Browser
 {
 
+    /**
+     * Path to the phantomJS executable
+     * @var string
+     */
     private $phantomExecutable;
 
+    /**
+     * Path to the netSniffing phantomJS script
+     * @var string
+     */
     private $netsniffScript;
 
+    /**
+     * Initialize a phantom object
+     *
+     * @param null $phantomPath
+     */
     public function __construct ($phantomPath = null)
     {
         $this->phantomExecutable = $phantomPath . 'bin/phantomjs';
         $this->netsniffScript = $phantomPath . 'examples/netsniff.js';
     }
 
+    /**
+     * Execute the phantomjs netsniffing script for a given URI
+     *
+     * @param array $parameters
+     * @return string
+     */
     private function execute (array $parameters)
     {
         $cmd = $this->phantomExecutable . ' ' . implode($parameters, ' ');
         return shell_exec($cmd);
     }
 
+    /**
+     * Create HAR archive from the sniffing result and return it.
+     *
+     * @param $url
+     * @return string
+     */
     public function createHttpArchive ($url)
     {
         $httpArchive = $this->execute(array(
