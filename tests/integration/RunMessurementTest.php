ad9fb961e90dc51ec4b578206ae5e84c3d827c82
Nils Langner <nils.langner@thewebhatesme.com>
2014-03-17 16:17:03 +0100

    Fixed integration tests. Runs if config is missing

--- tests/integration/RunMessurementTest.php
+++ tests/integration/RunMessurementTest.php
@@ -11,29 +11,23 @@ use Whm\Opm\Client\Command\SetupConfig;
 class RunMessurementTest extends PHPUnit_Framework_TestCase
 {
 
+    public function setUp ()
+    {
+        $configfile = __DIR__ . "/../../config.yml";
+        if (! file_exists($configfile)) {
+            copy(__DIR__ . "/config.yml", $configfile);
+        }
+    }
+
     public function testRunMessurement ()
     {
         $application = new Application();
         $application->add(new RunMessurement());
-
         $command = $application->find('runMessurement');
-
         $commandTester = new CommandTester($command);
 
-        $dialog = $command->getHelper('dialog');
-        $dialog->setInputStream($this->getInputStream('Test\n'));
-
-        $commandTester->execute(array('command' => $command->getName(), "config" => "config.yml"));
+        $commandTester->execute(array('command' => $command->getName(),"config" => "config.yml"));
 
         $this->assertTrue(true);
-    }
-
-    private function getInputStream ($input)
-    {
-        $stream = fopen('php://memory', 'r+', false);
-        fputs($stream, $input);
-        rewind($stream);
-
-        return $stream;
     }
 }
