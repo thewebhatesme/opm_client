1aed934afcdf9f4e9f86ee53555dc26cee82cc21
André Lademann <vergissberlin@googlemail.com>
2014-03-17 23:43:14 +0100

    Add documentation and exceptions in command and config section

--- src/Whm/Opm/Client/Command/ProcessUrl.php
+++ src/Whm/Opm/Client/Command/ProcessUrl.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the Open Performance Monitor Client package
+ *
+ * The Open Performance Monitor collects data to measure the performance of websites
+ *
+ * @package OPMCLient
+ */
+
 namespace Whm\Opm\Client\Command;
 
 use Whm\Opm\Client\Server\Server;
@@ -6,23 +14,46 @@ use Whm\Opm\Client\Config\Config;
 use Whm\Opm\Client\Browser\PhantomJS;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
 
+/**
+ * ProcessUrl
+ *
+ * Process an url and send the result (har file) to an opm server
+ *
+ * @category Command
+ * @package  OPMClient
+ * @license    https://raw.github.com/thewebhatesme/opm_server/master/LICENSE
+ * @example  $./bin/client processUrl
+ * @version   GIT: $Id$
+ * @since       Date: 2014-03-12
+ * @author    Nils Langner <nils.langner@phmlabs.com>
+ */
 class ProcessUrl extends Command
 {
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configure ()
     {
         $this->setName('processUrl')
-            ->setDescription('Process an url and send the result (har file) to an opm server.')
-            ->addArgument('config', InputArgument::REQUIRED, 'The config file')
+            ->setDescription('Process an url and send the result (har file) to an OPM server.')
+                ->addArgument('config', InputArgument::REQUIRED, 'The config file')
             ->addArgument('url', InputArgument::REQUIRED, 'The url that has to be fetched');
     }
 
-    protected function execute (InputInterface $input, OutputInterface $output)
-    {
+    /**
+     * Execute task
+     * 
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @throws DomainException
+     * 
+     * @return void 
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $config = Config::createFromFile($input->getArgument('config'));
 
         $phantom = new PhantomJS($config->getPhantomExecutable());
@@ -30,7 +61,10 @@ class ProcessUrl extends Command
 
         $server = new Server($config->getOpmServer(), $config->getClientId());
 
-        // @todo add try catch block
-        $server->addMessurement($input->getArgument('url'), $httpArchive);
+        try {
+            $server->addMessurement($input->getArgument('url'), $httpArchive);
+        } catch (\DomainException $e) {
+            throw $e;
+        }
     }
 }
--- src/Whm/Opm/Client/Command/RunMessurement.php
+++ src/Whm/Opm/Client/Command/RunMessurement.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * This file is part of the Open Performance Monitor Client package
+ *
+ * The Open Performance Monitor collects data to measure the performance of websites
+ *
+ * @package OPMCLient
+ */
+
 namespace Whm\Opm\Client\Command;
 
 use Whm\Opm\Client\Shell\BlockingExecutorQueue;
@@ -7,27 +16,61 @@ use Whm\Opm\Client\Server\Server;
 use Whm\Opm\Client\Config\Config;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
 
+/**
+ * ProcessUrl
+ *
+ * Process an url and send the result (har file) to an opm server
+ *
+ * @category Command
+ * @package  OPMClient
+ * @license    https://raw.github.com/thewebhatesme/opm_server/master/LICENSE
+ * @example  $./bin/client runMessurement
+ * @version   GIT: $Id$
+ * @since       Date: 2014-03-12
+ * @author    Nils Langner <nils.langner@phmlabs.com>
+ */
 class RunMessurement extends Command
 {
 
+    /**
+     * @var string Path to config file
+     */
     private $configFile;
 
+    /**
+     * @var string OPM Server URL
+     */
     private $server;
 
+    /**
+     * @var type
+     */
     private $blockingExecutorQueue;
 
-    protected function configure ()
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
     {
         $this->setName('runMessurement')
-            ->setDescription('Process an url and send the result (har file) to an opm server.')
-            ->addArgument('config', InputArgument::REQUIRED, 'The config file');
+                ->setDescription('Process an url and send the result (har file) to an opm server.')
+                ->addArgument('config', InputArgument::REQUIRED, 'The config file');
     }
 
-    protected function execute (InputInterface $input, OutputInterface $output)
+    /**
+     * Execute messurement task
+     *
+     * @example path description
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @throws \Whm\Opm\Client\Command\DomainException
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->configFile = $input->getArgument('config');
 
@@ -37,18 +80,30 @@ class RunMessurement extends Command
 
         $this->blockingExecutorQueue = new BlockingExecutorQueue($config->getMaxParallelRequests());
 
-        $this->processJob($this->server->getMessurementJob());
+        try {
+            $this->processJob($this->server->getMessurementJob());
+        } catch (\DomainException $e) {
+            throw $e;
+        }
     }
 
-    private function processJob (MessurementJob $job)
+    /**
+     * process the job
+     *
+     * @param \Whm\Opm\Client\Server\MessurementJob $job
+     * @todo Use *\Whm\Opm\Client\Config* object to build the cli command
+     *
+     * @return void
+     */
+    private function processJob(MessurementJob $job)
     {
         $urls = $job->getUrls();
-        $identifier = $job->getIdentifier();
 
         foreach ($urls as $url) {
-            $this->blockingExecutorQueue->addCommand("php bin/client.php processUrl " . $this->configFile . " " . $url);
+            $this->blockingExecutorQueue->addCommand("bin/client processUrl " . $this->configFile . " " . $url);
         }
 
         $this->blockingExecutorQueue->run();
     }
+
 }
