8e5ce5de5fad0bf30815ca8b5a7de2da48104d7f
Nils Langner <nils.langner@thewebhatesme.com>
2014-03-19 08:57:26 +0100

    Fixed uuit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
-include_once __DIR__.'/../vendor/autoload.php';
+$loader = include_once __DIR__ . "/../vendor/autoload.php";
+$loader->add("phmLabs", __DIR__ . "/../src/");
+$loader->add("Doctrine", __DIR__ . "/../src/");
+$loader->add("Whm", __DIR__ . "/../src/");
+$loader->add("Whm\\Test", __DIR__ . "/test/");
