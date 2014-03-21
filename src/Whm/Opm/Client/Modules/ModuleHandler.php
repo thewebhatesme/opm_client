d922baa89461e1baf940fba2be3ace4c87e12fff
Nils Langner <nils.langner@thewebhatesme.com>
2014-03-21 09:41:02 +0100

    Switched setup:phantomjs to new naming convention

--- src/Whm/Opm/Client/Modules/ModuleHandler.php
+++ src/Whm/Opm/Client/Modules/ModuleHandler.php
@@ -7,7 +7,7 @@ class ModuleHandler
 
     static public function getModules ()
     {
-        $modules = array ("Whm\Opm\Client\Modules\Messure\HttpArchive\Listener", "Whm\Opm\Client\Modules\Setup\PhantomJS\PhantomJs", "Whm\Opm\Client\Modules\Setup\Config\Config",
+        $modules = array ("Whm\Opm\Client\Modules\Messure\HttpArchive\Listener", "Whm\Opm\Client\Modules\Setup\PhantomJs\Listener", "Whm\Opm\Client\Modules\Setup\Config\Config",
                 "Whm\Opm\Client\Modules\Messure\HttpStatusOk\Listener");
 
         foreach ($modules as $module) {
